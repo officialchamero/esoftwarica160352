af704b49aca24d6d4c358165925df88975211ff8
Aneesh Shahi <chameroshahi@gmail.com>
2019-11-21 23:16:56 +0545

    commit during video

--- app/src/main/java/com/example/esoftwarica160352/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/esoftwarica160352/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragments");
+        mText.setValue("This is home fragmentss");
     }
 
     public LiveData<String> getText() {
